ec2170745e32549f9ae01c3adee1be73e004f20e
Pranav Shenoy <pranav.shenoy@utah.edu>
2023-02-07 18:40:52 -0800

    mnr

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -103,6 +103,48 @@
 // Has to be the last include (doesn't have include guards):
 #include "src/objects/object-macros.h"
 
+bool Timer::started() {
+  return started_;
+}
+
+void Timer::start(const std::function<void()>& f, time_t::duration interval) {
+  std::lock_guard<std::recursive_mutex> timer_guard(mutex);
+  assert(!started());
+  started_ = true;
+  t = std::thread([=](){
+    while(this->started_) {
+      time_t time = std::chrono::system_clock::now();
+      f();
+      std::this_thread::sleep_until(time + interval);
+    }
+  });
+}
+
+void Timer::try_start(const std::function<void()>& f, time_t::duration interval) {
+  if (!started()) {
+    start(f, interval);
+  }
+}
+
+void Timer::stop() {
+  std::lock_guard<std::recursive_mutex> timer_guard(mutex);
+  CHECK(started());
+  started_ = false;
+  CHECK(t.joinable());
+  t.join();
+}
+
+void Timer::try_stop() {
+  std::lock_guard<std::recursive_mutex> timer_guard(mutex);
+  if (started()) {
+    stop();
+  }
+}
+
+
+
+
+
 using namespace nlohmann;
 namespace v8 {
 namespace internal {
